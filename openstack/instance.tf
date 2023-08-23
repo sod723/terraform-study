1a06baf83c51ecd124e59f9ac338b28780b451f1
sod723 <sod723@naver.com>
2023-08-23 16:04:55 +0900

    volume

--- openstack/instance.tf
+++ openstack/instance.tf
@@ -1,3 +1,12 @@
+#볼륨추가
+resource "openstack_blockstorage_volume_v3" "myvol" {
+  region = "RegionOne"
+  name = "myvol"
+  size = 20
+  image_id = "97d89ac9-c47c-4e69-b89b-9a9b398893a6"
+}
+
+#인스턴스
 resource "openstack_compute_instance_v2" "basic-instance01" {
   name = "basic-instance01"
   image_id = "97d89ac9-c47c-4e69-b89b-9a9b398893a6"
